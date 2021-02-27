d21a7570c3028e51af7bcfb1e210059c7470697c
Ling <ling_y@web.de>
2021-02-27 18:32:29 +0100

    Navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-end">
+          <a class="button is-primary">
+            <strong>+</strong>
+          </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
